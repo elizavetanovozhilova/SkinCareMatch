8fe6095d9930e18e1e05de14beccc18c5051545c
aminagazizova <aminagazizova8@gmail.com>
2024-07-09 16:43:26 +0300

    добавление таб бар

---
+++ SkinCareMatch/TabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarViewController.swift
+//  SkinCareMatch
+//
+//  Created by Амина Газизова on 09.07.2024.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
